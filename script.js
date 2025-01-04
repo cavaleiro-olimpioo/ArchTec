caf3245e35ce0b293d91c581d2c7541c35a5357e
cavaleiro-olimpioo <applehunters365@gmail.com>
2025-01-04 13:53:54 -0300

    Pop-upTST

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function menuOff(){
     let menu = document.querySelector('div.menu') 
     menu.style.display = 'none'
 }
+
+function closePop(){
+    let pop = document.querySelector('div.popup')
+    pop.style.display = 'none'
+    let over = document.querySelector('div.popup-overlay')
+    over.style.display = 'none'
+}
