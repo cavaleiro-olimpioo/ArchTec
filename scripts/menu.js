302cb4badafd2f69432f4459af71da2c7b905b56
cavaleiro-olimpioo <applehunters365@gmail.com>
2025-01-25 21:20:31 -0300

    AnimacaoV1
    Animacao dos menus e do popup =D

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,16 @@
-function menuOn(){
-    let menu = document.querySelector('div.menu')
-    menu.style.display = 'block'
-}
-
-function menuOff(){
-    let menu = document.querySelector('div.menu') 
-    menu.style.display = 'none'
+function menu() {
+    const menu = document.querySelector('div.menu');
+    const smb = document.querySelector('button.togglemenu');
+    
+    // Verifica se o menu já está visível
+    if (menu.classList.contains('show')) {
+        menu.classList.remove('show');  // Remove a classe 'show' para esconder
+        menu.classList.add('hide');     // Adiciona a classe 'hide' para mover para fora
+    } else {
+        menu.classList.remove('hide');  // Remove a classe 'hide' para iniciar a animação
+        menu.classList.add('show');     // Adiciona a classe 'show' para mostrar o menu
+    }
+    
+    // Adiciona ou remove a classe 'x' para a animação do botão
+    smb.classList.toggle('x');
 }
